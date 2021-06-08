98217da93294c2cee7fa2952202f02e68bf96897
Denise Dube <denise.d.dube@gmail.com>
2021-06-08 16:37:52 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- src/main/example.ts
+++ src/main/example.ts
@@ -1,10 +1,10 @@
 export class Example {
     fib(a: number) {
-        if(a===1){
+        if (a === 1) {
             return 1;
         }
-        if(a===2){
-            return 1;
+        if (a > 1) {
+            return a - 1;
         }
         return 0;
     }
--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -3,16 +3,17 @@ import {Example} from "../main/example"
 
 describe('example test', () => {
     const generator = new Example();
-    it.each([[0,0],
-        [1,1],
-        [2,1],
-        [3,2]
+    it.each([[0, 0],
+        [1, 1],
+        [2, 1],
+        [3, 2],
+        [4, 3],
+        [5, 5]
     ])
-    ('For index %d return %d', (index,returnValue) => {
+    ('For index %d return %d', (index, returnValue) => {
         expect(generator.fib(index)).toBe(returnValue);
     })
 })
-
 
 
 //0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811
